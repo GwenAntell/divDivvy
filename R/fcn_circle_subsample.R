d0d9bbaaa663a7fdb6cfedc9fc3f065a0e109ecb
Gwen Antell <46300365+GwenAntell@users.noreply.github.com>
2022-03-31 13:31:55 +0100

    define fcns to get all cells in buffer and find viable seed cells

--- R/fcn_circle_subsample.R
+++ R/fcn_circle_subsample.R
@@ -1,19 +1,11 @@
 library(terra)
 library(sf)
-
-# Use cell number/site ID and centroid coords as input.
-# (Strictly, these don't have to be raster cell coords - could be coords of
-# a predefined/clean set of locality points.)
-# dat should contain unique site/cell IDs and locations
-# (otherwise add step to subset pool to unique IDs)
-
 # TODO option to pass on prj info to relevant function guts
 
-# function of 1 subsample (1 try on 1 starting cell within 1 bin)
-# take a subsample of sites/cells, without replacement, within buffered radius
-cookie <- function(dat, siteId, xy, r, nSite, # prj,
-                   seed, weight = FALSE # wasn't already an argument but should be
-                 ){
+# return vector of cells that lie within buffer radius of given seed
+# internal fcn for findPool and cookie
+findPool <- function(seed, dat, siteId, xy, r, nSite # , prj
+                     ){
   seedRow <- which(dat[, siteId] == seed)[1]
   # make sure coords are a 2-col, not-dataframe-class object to give to sf
   seedxy <- as.matrix( dat[seedRow, xy] )
@@ -21,25 +13,56 @@ cookie <- function(dat, siteId, xy, r, nSite, # prj,
   # make sure to hard code the CRS for the buffer; sf can't infer lat-long
   seedsfc <- st_sfc(seedpt, crs = 'epsg:4326')
   buf <- st_buffer(seedsfc, dist = r*1000) 
-  # also works:
-  # rangemap::geobuffer_points(dat[1:5,xy], r*1000, wrap_antimeridian = TRUE)
-  
-  # split poly into multipolygon around antimeridian
+  # split poly into multipolygon around antimeridian (patches 2020 bug)
   bufWrap <- st_wrap_dateline(buf, options = c("WRAPDATELINE=YES"))
-  # examples apply st_union on this output, but seems unnecessary in tests here
+  # identical results if st_union() applied to bufWrap
+  # alternative way to construct buffer, but adding new pkg dependency:
+  # rangemap::geobuffer_points(seedxy, r*1000, wrap_antimeridian = TRUE)
   
   # find sites within radius of seed site/cell
   datSf <- st_as_sf(dat, coords = c('long','lat'),
                     crs = 'epsg:4326')
-  poolBool <-  st_intersects(datSf, st_union(bufWrap), 
-                             sparse = FALSE)
-  # st_intersection would return geometries instead of logicals
-  if (sum(poolBool) < nSite){
+  poolBool <-  st_intersects(datSf, bufWrap, sparse = FALSE)
+  pool <- dat[poolBool, siteId]
+  return(pool)
+}
+
+# function to try all possible starting pts (i.e. all occupied cells)
+# save the ID of any cells that contain given pool size within buffer
+findSeeds <- function(dat, siteId, xy, r, nSite, # prj
+                      ){
+  # count unique sites (not taxon occurences) relative to subsample quota
+  dupes <- duplicated(dat[,siteId])
+  dat <- dat[ !dupes, ]
+  
+  # test whether each occupied site/cell is viable for subsampling
+  posSeeds <- dat[,siteId]
+  isOK <- sapply(posSeeds, function(s){
+    sPool <- findPool(s, dat, siteId, xy, r, nSite)
+    n <- length(sPool)
+    ifelse(n > nSite, s, NA)
+  })
+  # return IDs of only those seeds that are viable (pool > nSite)
+  viable <- isOK[ !is.na(isOK)]
+  return(viable)
+}
+
+# Use cell number/site ID and centroid coords as input.
+# (Strictly, these don't have to be raster cell coords - could be coords of
+# a predefined/clean set of locality points.)
+# dat should contain unique site/cell IDs and locations
+# (otherwise add step to subset pool to unique IDs)
+
+# function of 1 subsample (1 try on 1 starting cell within 1 bin)
+# take a subsample of sites/cells, without replacement, within buffered radius
+cookies <- function(dat, siteId, xy, r, nSite, # prj,
+                    seed, weight = FALSE){
+  pool <- findPool(seed, dat, siteId, xy, r, nSite)
+  if (length(pool) < nSite){
     stop('not enough pool cells to sample')
   }
-  pool <- dat[poolBool, siteId]
   
-  # assign sampling weights return ID names of rarefied sites/cells
+  # assign sampling weights and return IDs of rarefied sites/cells
   if (weight){  
     samplIds <- sample(sample(pool), nSite, replace = FALSE) 
     # extra random sampling
@@ -55,7 +78,7 @@ cookie <- function(dat, siteId, xy, r, nSite, # prj,
     # squared inverse weight because inverse alone is too weak an effect
     gcdists <- st_distance(poolPts, seedsfc) # spherical distances by default
     wts <- sapply(gcdists, function(x) x^(-2))
-    # it's ok weights don't sum to 1; sample() doesn't require this
+    # sample() doesn't require wts sum = 1; identical results without rescaling
     samplIds <- c(seed, 
                   sample(sample(pool), nSite-1, prob = wts, replace = FALSE)
                   )
