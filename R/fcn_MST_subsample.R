bfecdfde93e3a878a21090ac8c6319a26c673e0d
Gwen Antell <46300365+GwenAntell@users.noreply.github.com>
2022-04-01 08:39:18 +0100

    match same vars/dependencies/output as circle subsample fcn

--- R/fcn_MST_subsample.R
+++ R/fcn_MST_subsample.R
@@ -1,7 +1,6 @@
 # function to find all nested nearest-neighbour spatial subsamples
 # modified from Roger Close's findAllNestedSpatialSubsamples.R
-library(fields)
-library(purrr) # for flatten(), but could write out longhand to reduce dependencies
+library(sf)
 
 # TODO discuss -
 # * What should the output be? what format - list instead of tibble?
@@ -12,115 +11,69 @@ library(purrr) # for flatten(), but could write out longhand to reduce dependenc
 #   (e.g. list[i] = {1,2,3}, {1,2,3,4}, ... {1,2,3,...,n})
 
 # for testing purposes
-  # setwd("D:/Dropbox/OxfordPDRA")
-  # datFls <- list.files('./data') %>% 
-  #   grep('csv', ., value = TRUE)
-  # pbdb_data <- paste0('data/', datFls[3]) %>%
-  #   read.csv(header = TRUE, skip = 19)
-  # lng_var <- "paleolng"
-  # lat_var <- "paleolat"
-  # min_n <- 3
-  # max_branch <- 1000
-  # i <- 1 # index ID of unique locations
+  datFls <- list.files('./data') %>%
+    grep('csv', ., value = TRUE)
+  dat <- paste0('data/', datFls[3]) %>%
+    read.csv(header = TRUE, skip = 19)
+  xVar <- "paleolng"
+  yVar <- "paleolat"
+  nMin <- 3
+  branchMax <- 1500
 
-# findAllNestedSpatialSubsamples # original function name
-ptClustr <- function(pbdb_data, lng_var = 'paleolng', lat_var = 'paleolat',
-                      # paleolngvar = "paleolng", paleolatvar = "paleolat", 
-                     min_n = 3, max_branch = 1000 # max.subtree.size = 10000, 
-                     # ID = "", # ID is bin name in RC script
-                     # n_cores = n_cores # use this if adding option for parallel
+ptClustr <- function(dat, xVar = 'paleolng', yVar = 'paleolat',
+                     nMin = 3, branchMax = 1000 # max.subtree.size = 10000, 
                      ) {
-  # RC: the bin name is fed to the function simply to add an ID string to spatial subsamples in output
-  
-	# set.seed(666)
-  # TODO why set a seed? there aren't any random components
-  # results identical between seed 6 and 666
-  
-  # ensure min number of occs, subset to unique locations, find GCD between all
-	pcoords <- pbdb_data[,c(lng_var, lat_var)]
-	colnames(pcoords) <- c('paleolng', 'paleolat')
-	pbdb_data$pcoords <- paste(pbdb_data[[lng_var]], pbdb_data[[lat_var]], sep = "/")
-	pcoords <- distinct(pcoords) %>% .[complete.cases(.), ] %>% as.data.frame()
-
-	if (nrow(pcoords) < min_n) {
-		stop('insufficient points for a cluster')
-	  # return(NULL)
+  # subset to unique locations and find dist matrix between all
+#	dat$pcoords <- paste(dat[[xVar]], dat[[yVar]], sep = '/') # not used again?
+	xy <- dat[,c(xVar, yVar)]
+	dupes <- duplicated(xy)
+	uniq <- xy[ !dupes, ]
+	pcoords <- uniq[complete.cases(uniq), ] 
+	pcoords <- as.data.frame(pcoords) # in case data is given as a matrix
+	# TODO only assign id if not already given? (e.g. cell name)
+	pcoords$ID <- paste('loc', 1:nrow(pcoords), sep = "") 
+#	pcoords <- pcoords[,c('ID', xVar, yVar)] # isn't it already only 3 cols??
+	nLoc <- nrow(pcoords)
+	if (nLoc < nMin) {
+	  stop('insufficient points for a cluster')
 	}
-
-	pcoords$ID <- paste("loc", 1:nrow(pcoords), sep = "")
-	            # paste(ID, "loc", 1:nrow(pcoords), sep = "")
-	pcoords <- pcoords[,c("ID","paleolng","paleolat")]
-	nloc <- nrow(pcoords)
-
-	gcd_matrix <- fields::rdist.earth(pcoords[,c("paleolng","paleolat")], miles = FALSE)
-	# RC: One minor error in the function is that I didn't explicitly specify 
-	# great circle distances computed by fields::rdist.earth() should be in km, 
-	# so the sizes of spatial samples in the Science paper should be in miles
-	colnames(gcd_matrix) <- rownames(gcd_matrix) <- pcoords$ID
-	diag(gcd_matrix) <- NA
-
-# find closest point, add it to set, and repeat until reach max tree branch
-	ssFun <- function(i){  
-		ss <- list()
-		set <- rownames(gcd_matrix)[i]
-		for (j in 1:(nloc)) {
+  
+	coordSf <- st_as_sf(pcoords, coords = c(xVar, yVar),
+	                     crs = 'epsg:4326')
+	gcdists <- st_distance(coordSf) # spherical distances (m) by default
+	colnames(gcdists) <- rownames(gcdists) <- pcoords$ID
+	diag(gcdists) <- NA
+  
+  # find closest point, add it to set, and repeat until reach max tree branch
+	groupr <- function(id){
+#		ss <- list() # GSA edit: only return max cluster, not every clust from 3:max points
+		clust <- id
+		for (j in 1:(nLoc)) {
 	  #	find distance from each point in the set to all remaining points
-			posscon <- gcd_matrix[which(pcoords$ID %in% set),
-			                      -which(pcoords$ID %in% set), drop = F]
+			posscon <- gcdists[ which(pcoords$ID %in% clust),
+			                   -which(pcoords$ID %in% clust), drop = F]
 		# then retrieve name of closest next point
 			nearest <- names(which.min(apply(posscon, MARGIN = 2, min)))
-			set <- unique(c(set, nearest))
+			clust <- unique(c(clust, nearest))
 			
-		#	set <- set[!is.na(set)] 
-			if (set %>% is.na %>% any) stop('NA name in points set!')
-			# flag an error if ever an NA value.
-			# seems like there should never be, 
-			# but RC must've added this check for a reason
-			
-			ss[[j]] <- set
-			lngBranch <- max(gcd_matrix[which(pcoords$ID %in% set), 
-			                            which(pcoords$ID %in% set)], na.rm = T)
-			if (lngBranch > max_branch) break
+#			ss[[j]] <- clust
+			lngBranch <- max(gcdists[which(pcoords$ID %in% clust), 
+			                         which(pcoords$ID %in% clust)], na.rm = T)
+			units(branchMax) <- 'm'
+			if (lngBranch > branchMax * 1000) break
 		}
-		return(ss)
-	  } 
-	ss <- lapply(1:nloc, ssFun)
-	# if mc option specified with n_cores,
-	# ss <- mclapply(seq_len(nrow(gcd_matrix)), ssFun,
-	#	               mc.cores = n_cores, mc.preschedule = T, mc.cleanup = T, mc.silent = T)
+		sort(clust) # return(ss)
+	}
 	
-	# flatten() removes 1 hierarchy level to squash list of lists into vector
-	ss1 <- flatten(ss) %>% lapply(., sort) %>% unique
-	
+	ss <- sapply(pcoords$ID, groupr)
+	ss1 <- unique(ss)
+  
 	# weed out any clusters with fewer than min specified points
-	ss1 <- ss1[sapply(ss1, length) >= min_n]
+	ss1 <- ss1[sapply(ss1, length) >= nMin]
 	if (length(ss1) == 0){
 	  # return(NULL)
-	  stop('no set of points close enough to form a cluster')
+	  stop('not enough close sites for any sample')
 	} 
 	
-	# back-calculate a df of lat-long coords for each cluster of names
-	ss2 <- lapply(ss1, function(x) pcoords[which(pcoords$ID %in% x), ])
-	names(ss2) <- paste("SS", 1:length(ss2), sep = "")
-	           # paste(ID, "SS", 1:length(ss2), sep = "")
-
-	# retrieve IDs of collections located at each cluster's pts
-	# (note that multiple, unique collections can be at same point)
-	coll_nos <- lapply(ss2, function(x) {
-		temp <- paste(x$paleolng, x$paleolat, sep = "/")
-		temp2 <- pbdb_data[which(pbdb_data$pcoords %in% temp), ]
-		coll_nos <- unique(temp2$collection_no)
-		return(coll_nos)
-	})
-
-	gcd_matrices <- lapply(ss2, function(x) {
-		gcd_matrix[x$ID, x$ID]
-	})
-  
-	# return everything that might be useful to query later
-	ss_df <- tibble(SSID = names(ss2), SS = ss2, 
-	                gcd_matrix = gcd_matrices, collection_nos = coll_nos)
-
-	return(ss_df)
-
+	return(ss1)
 }
