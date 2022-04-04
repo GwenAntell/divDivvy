927675c24cdb86da184f55f10b6a33132b55e144
Gwen Antell <46300365+GwenAntell@users.noreply.github.com>
2022-04-04 12:59:50 +0100

    init fcns to calculate all summary variables on a sample

---
+++ R/fcn_calc_div_variables.R
@@ -0,0 +1,129 @@
+library(iNEXT)
+library(units)
+
+dat <- sites
+taxVar <- 'genus'
+idVar <- 'face' # rename to 'siteVar' or 'sampVar' ?
+sampIds <- circs[[1]]
+collections <- 'collection_no' # remove as too problematic a variable?
+coords <- c('long','lat') 
+# c('paleolng','paleolat') # if using original, vector coords
+quotaQ <- 0.2
+quotaN <- 10
+omitDom <- TRUE
+
+smry <- sampMeta(dat = df, taxVar = 'accepted_name', 
+                 idVar = 'siteID', sampIds = circs[[1]],
+                 collections = 'collection_no', 
+                 coords = c('paleolng','paleolat'),
+                 quotaQ = 0.15, quotaN = 10)
+
+# collections arg = name of col containing collection id, e.g. 'collection_no'
+# coords arg = name of latlong cols to calculate lat range, MST, and midpoint
+# quotaN = if given, perform classical rarefaction w specified quota (n occs)
+# quotaQ = if given, perform SQS w specified coverage/quorum level
+# omitX = removes single/dominant species from ALL metrics
+
+# return: great circle distance and summed tree length is returned in km
+# if too few taxon occs to achieve specified rarefaction level, div is extrap
+
+sampMeta <- function(dat, taxVar, idVar, sampIds,
+                     collections = NULL, coords = NULL,
+                     quotaQ = NULL, quotaN = NULL, 
+                     omitDom = FALSE){
+  smpld <- dat[, idVar] %in% sampIds
+  datSamp <- dat[smpld,]
+  out <- c()
+  # n. collections (PBDB data) may be useful to know if rarefying
+  if ( !is.null(collections) ){
+    collSamp <- unique( datSamp[,collections] )
+    out <- cbind(out, 'nColl' = length(collSamp))
+  }
+  # comb out any duplicate occurrences of a taxon w/in single site.
+  # do this after counting collections in case a single taxon
+  # at a single site is recorded in multiple collections
+  if (is.null(coords)){
+    datSamp <- unique( datSamp[,c(taxVar, idVar)] )
+  } else {
+    datSamp <- unique( datSamp[,c(taxVar, idVar, coords)] )
+  }
+  
+  siteSamp <- unique( datSamp[,idVar])
+  nSite <- length(siteSamp)
+  out <- cbind(out, 'nSite'= nSite)
+
+  # run range size fcn as if all occs were from a single taxon
+  # duplicate localities affect only occ count, nothing else
+  spatMeta <- rangeSizer(datSamp, coords = coords)
+  out <- cbind(out, spatMeta)
+  
+  # diversity metrics; optional coverage and/or classical rarefaction
+  taxSamp <-  unique( datSamp[,taxVar] )
+  out <- cbind(out, 'nTax' = length(taxSamp)) # raw count of taxa
+  freqs <- table( datSamp[,taxVar] )
+  freqOrdr <- sort( as.numeric(freqs), decreasing = TRUE)
+  if (omitDom == TRUE){
+    dom <- names( which.max(freqs) )
+    domRows <- datSamp[,taxVar] == dom
+    datSamp <- datSamp[ !domRows, ]
+  }
+  if ( !is.null(quotaQ) ){
+    sqsFull <- estimateD(list( c(nSite, freqOrdr) ), 
+                         datatype = 'incidence_freq',
+                         base = 'coverage', level = quotaQ # , conf=NULL # 95% CI or none
+                         )
+    # q0 = richness, q1 = exp of Shannon's entropy index, 
+    # q2 = inverse of Simpson's concentration index
+    sqsRich <- sqsFull[sqsFull$order == 0, c('SC','qD','qD.LCL','qD.UCL')]
+    names(sqsRich) <- c('u','SQSdiv','SQSlow95','SQSupr95')
+    # return sample coverage, richness estimate and 95% CI
+    out <- cbind(out, sqsRich)
+  }
+  if ( !is.null(quotaN) ){
+    crFull <- estimateD(list( c(nSite, freqOrdr) ), 
+                        datatype = 'incidence_freq',
+                        base = 'size', level = quotaN # , conf=NULL # 95% CI or none
+                        )
+    crRich <- crFull[crFull$order == 0, c('qD','qD.LCL','qD.UCL')]
+    names(crRich) <- c('CRdiv','CRlow95','CRupr95')
+    out <- cbind(out, crRich)
+  }
+  return(out)
+}
+
+# dat should be list of occs for a single taxon, which are presumed unique
+rangeSizer <- function(dat, coords = NULL){
+  out <- cbind('nOcc' = nrow(dat))
+  if ( !is.null(coords) ){
+    xy <- dat[,coords]
+    latDiff <- max(xy[,2]) - min(xy[,2])
+    latRng <- abs(latDiff)
+    pts <- st_as_sf(xy, coords = coords, crs = 'epsg:4326')
+    ptsGrp <- st_union(pts)
+    cntr <- unlist( st_centroid(ptsGrp) )
+    gcdists <- st_distance(pts) # returns units-class object (m)
+    gcMax <- max(gcdists) / 1000
+    mst <- spantree( drop_units(gcdists) )
+    agg <- sum(mst$dist) / 1000 
+    out <- cbind(out,
+                 'centroidLng' = cntr[1],
+                 'centroidLat' = cntr[2],
+                 'latRange' = latRng,
+                 'greatCircDist' = gcMax,
+                 'minSpanTree' = agg
+    )
+  }
+  return(out)
+}
+
+# TODO return range sizes for all species in all subsamples
+taxDists <- function(dat, taxVar, idVar, sampIds,
+                     coords = NULL, omitSingles = FALSE){
+  if (omitSingles == TRUE){
+    freqs <- table( dat[,taxVar] )
+    singles <- names(freqs[freqs == 1])
+    rows2toss <- dat[,taxVar] %in% singles
+    dat <- dat[ !rows2toss, ]
+  }
+  # apply rangeSizer to all taxa
+}
