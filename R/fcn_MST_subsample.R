40cc7e605f2e3cdfa16dd37e2e8c1d59cdc5c73d
Gwen Antell <gwenantell@gmail.com>
2022-04-05 11:32:05 +0100

    apply fcn on random start pts instead of exhaustively

--- R/fcn_MST_subsample.R
+++ R/fcn_MST_subsample.R
@@ -2,45 +2,47 @@
 # modified from Roger Close's findAllNestedSpatialSubsamples.R
 library(sf)
 
-# TODO discuss -
-# * Should branch cutoff be before or after max threshold reached?
-# * Save only max cluster, not *everything* from min pts until max branch reached?
+# Changes to original (published) code -
+# * Branch cutoff made *before* max threshold reached, not 1 point after
+# * Save only max cluster, not *everything* from min pts until max branch reached
 #   (e.g. list[i] = {1,2,3}, {1,2,3,4}, ... {1,2,3,...,n})
+# * Start tree at random point and repeat for specified n iterations
+#   (vs. exhaustively building at every possible start point)
 
 # for testing purposes
   # datFls <- list.files('./data') %>%
   #   grep('csv', ., value = TRUE)
-  # dat <- paste0('data/', datFls[3]) %>%
-  #   read.csv(header = TRUE, skip = 19)
+  # dat <- paste0('data/', datFls[1]) %>% read.csv
+  # keepRows <- complete.cases(dat[,c('paleolng','paleolat','genus')])
+  # dat <- dat[keepRows,]
   # xy <- c('paleolng', 'paleolat')
   # nMin <- 3
-  # branchMax <- 1500
+  # branchMax <- 1500 # in km
+  # iter <- 500 # should be > 1 for sample() to work correctly
 
 ptClustr <- function(dat, xy = c('paleolng', 'paleolat'),
-                     nMin = 3, branchMax = 1000 # max.subtree.size = 10000, 
+                     nMin = 3, branchMax, iter 
                      ) {
   # subset to unique locations and find dist matrix between all
 	coords <- dat[,xy]
 	dupes <- duplicated(coords)
 	coords <- coords[ !dupes, ]
-	coords <- uniq[complete.cases(coords), ] 
+	coords <- coords[complete.cases(coords), ] 
 	coords <- as.data.frame(coords) # in case data is given as a matrix
 	# TODO only assign id if not already given? (e.g. cell name)
-	coords$ID <- paste('loc', 1:nrow(coords), sep = "") 
+	coords$ID <- paste('loc', 1:nrow(coords), sep = '') 
 	nLoc <- nrow(coords)
 	if (nLoc < nMin) {
 	  stop('insufficient points for a cluster')
 	}
   
-	coordSf <- st_as_sf(coords, coords = c(xVar, yVar),
-	                    crs = 'epsg:4326')
+	coordSf <- st_as_sf(coords, coords = xy, crs = 'epsg:4326')
 	gcdists <- st_distance(coordSf) # spherical distances (m) by default
 	colnames(gcdists) <- rownames(gcdists) <- coords$ID
 	diag(gcdists) <- NA
   
   # find closest point, add it to set, and repeat until reach max tree branch
 	groupr <- function(id){
-#		ss <- list() # GSA edit: only return max cluster, not every clust from 3:max points
 		clust <- id
 		for (j in 1:(nLoc)) {
 	  #	find distance from each point in the set to all remaining points
@@ -48,22 +50,25 @@ ptClustr <- function(dat, xy = c('paleolng', 'paleolat'),
 			                   -which(coords$ID %in% clust), drop = F]
 		# then retrieve name of closest next point
 			nearest <- names(which.min(apply(posscon, MARGIN = 2, min)))
-			clust <- unique(c(clust, nearest))
-			
-#			ss[[j]] <- clust
-			lngBranch <- max(gcdists[which(coords$ID %in% clust), 
-			                         which(coords$ID %in% clust)], na.rm = T)
-			units(branchMax) <- 'm'
-			if (lngBranch > branchMax * 1000) break
+			canddt <- unique(c(clust, nearest))
+			# check if adding next pt puts cluster over size limit - if so don't add
+			lngBranch <- max(gcdists[which(coords$ID %in% canddt), 
+			                         which(coords$ID %in% canddt)], na.rm = T)
+			branchMax <- set_units(branchMax, 'km')
+			if (lngBranch > branchMax){
+			  break
+			}
+			clust <- canddt
 		}
-		sort(clust) # return(ss)
+		sort(clust)
 	}
 	
-	ss <- sapply(coords$ID, groupr)
-	ss1 <- unique(ss)
+	seeds <- sample(sample(coords$ID), iter, replace = TRUE)
+	ss <- sapply(seeds, groupr)
+#	ss1 <- unique(ss) # duplicate subsample entities allowed, matches circ fcn
   
 	# weed out any clusters with fewer than min specified points
-	ss1 <- ss1[sapply(ss1, length) >= nMin]
+	ss1 <- ss[sapply(ss, length) >= nMin]
 	if (length(ss1) == 0){
 	  stop('not enough close sites for any sample')
 	} 
