3b5e58bb7cc8fcfcc96ea10f9264b52d21a2cc9a
Gwen Antell <46300365+GwenAntell@users.noreply.github.com>
2022-05-04 17:03:02 +0100

    patch in case multiple points equally closest to cluster

--- R/subsample_MST.R
+++ R/subsample_MST.R
@@ -1,17 +1,5 @@
-# function to find all nested nearest-neighbour spatial subsamples
+# functions to find all nested nearest-neighbour spatial subsamples
 # modified from Roger Close's findAllNestedSpatialSubsamples.R
-
-# Changes to original (published) code -
-# * Branch cutoff made *before* max threshold reached, not 1 point after
-# * Save only max cluster, not *everything* from min pts until max branch reached
-#   (e.g. list[i] = {1,2,3}, {1,2,3,4}, ... {1,2,3,...,n})
-# * Start tree at random point and repeat for specified n iterations
-#   (vs. exhaustively and uniquely building at every point)
-
-# for a lot of paleo datasets, there will be fewer unique sites
-# than desired number of iterations, so more efficient to build every possible tree
-# up front and then (re)sample them n iterations, instead of
-# recalculating a new tree each iteration
 
 # find closest point, add it to set, and repeat until reach max tree branch
 # distMtrx arg = pairwise distances with units, rownames and colnames are pt IDs
@@ -25,8 +13,15 @@ groupr <- function(seed, sfPts, distMtrx, distMax){
     distsOut <- distMtrx[ which(ptIds %in% clust),
                          -which(ptIds %in% clust), drop = F]
     # then retrieve name of closest next point
-    nearest <- names(which.min(apply(distsOut, MARGIN = 2, min)))
-    canddt <- c(clust, nearest) # unique
+    mins <- apply(distsOut, MARGIN = 2, min)
+    minMin <- min(mins)
+    nearest <- names( which(mins == minMin) )
+    # NB which.min() finds FIRST occurrence of min value, so instead:
+    # hard-code to randomly pick in rare case of ties for min
+    if (length(nearest) > 1){
+      nearest <- sample(nearest, 1)
+    }
+    canddt <- c(clust, nearest)
     # check if adding next pt puts cluster over size limit - if so don't add
     diam <- max(distMtrx[which(ptIds %in% canddt),
                          which(ptIds %in% canddt)], na.rm = T)
@@ -45,7 +40,7 @@ groupr <- function(seed, sfPts, distMtrx, distMax){
 #'
 #' Lagomarcino and Miller (2012) developed an iterative approach of aggregating
 #' localities to build clusters based on convex hulls, inspired by species-area
-#' curve analysis (Scheiner 2003). Close et al. (2020) refined the approach and
+#' curve analysis (Scheiner 2003). Close et al. (2017, 2020) refined the approach and
 #' changed the proximity metric from minimum convex hull area to minimum spanning
 #' tree length. The present implementation adapts code from Close et al. (2020)
 #' to add an option for site rarefaction after cluster construction and to grow
@@ -59,6 +54,8 @@ groupr <- function(seed, sfPts, distMtrx, distMax){
 #' cluster is grouped in next, and its distance (branch) is added to the sum
 #' tree length. This iterative process continues until the largest distance
 #' between any two points in the cluster would exceed \code{distMax} km.
+#' In the very rare case multiple candidate points are tied for minimum distance
+#' from the cluster, one point is selected at random as the next to include.
 #' Any tree with fewer than \code{nMin} points is prohibited.
 #'
 #' In the case that \code{nSite} is supplied, \code{nMin} argument is ignored,
@@ -93,6 +90,8 @@ groupr <- function(seed, sfPts, distMtrx, distMax){
 #' @references
 #'
 #' \insertRef{Antell2020}{divDivvy}
+#'
+#' \insertRef{Close2017}{divDivvy}
 #'
 #' \insertRef{Close2020}{divDivvy}
 #'
