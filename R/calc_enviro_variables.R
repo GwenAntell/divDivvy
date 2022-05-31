9f155c4255d7e4910631fec276e9f90180ff00d4
Gwen Antell <gwenantell@gmail.com>
2022-05-31 14:33:25 +0100

    init commit fcn to categorise grid cell habitat

---
+++ R/calc_enviro_variables.R
@@ -0,0 +1,50 @@
+# values of unknown env should be specified as NA, not e.g. 'unknown'
+# dat can be df in which case xy and env should be col names
+# or, omit dat arg and xy should be coords, env should be vector
+# cutoff is inclusive, e.g. values >= cutoff will be assigned to class
+envirise <- function(grid, dat = NULL, xy, env, cutoff){
+  if (! is.null(dat)){
+    xy  <- dat[, xy]
+    env <- dat[, env]
+  }
+  naEnv <- is.na(env)
+  env <- env[!naEnv]
+  xy  <-  xy[!naEnv,]
+
+  # tally occurrences with each enviro within raster cells
+  lvls <- unique(env)
+  lyrL <- lapply(lvls, function(lvl){
+    xyMat <- data.matrix( xy[env==lvl,] )
+    rasterize(xyMat, grid, fun = length)
+  }  )
+  lyrs <- rast(lyrL)
+  names(lyrs) <- lvls
+
+  if (cutoff <= 0.5){ stop('cutoff must be greater than 0.5') }
+  # set NA values to 0 so when summed across layers, result is not NA
+  reclass <- matrix(c(NA, 0), ncol = 2)
+  lyrsNoNA <- classify(lyrs, reclass)
+  prop <- lyrs / sum(lyrsNoNA) # convert to proportion
+  clasfy <- rbind(c(0, cutoff, 0), # binarise
+                  c(cutoff, 1, 1))
+  # values = cutoff classified as above the cutoff (left-closed interval)
+  mjtys <- classify(prop, clasfy,
+                    include.lowest = TRUE,
+                    right = FALSE)
+  values(grid) <- NA # ensure blank starting raster
+  for (l in 1:length(lvls)){
+    grid[mjtys[[l]]==1] <- l
+  }
+
+  # change NA cells to indet. if they do have occurrence data
+  # but no one environmental class exceeds cutoff
+  anydat <- rasterize(data.matrix(xy), grid)
+  anydat <- classify(anydat, cbind(1, length(lvls)+1))
+  grid <- cover(grid, anydat)
+
+  lvls <- c(lvls, 'indet.')
+  attrs <- data.frame('num' = 1:length(lvls))
+  attrs$mainClass <- lvls
+  levels(grid) <- attrs
+  return(grid)
+}
