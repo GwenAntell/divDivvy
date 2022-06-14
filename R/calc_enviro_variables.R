7e2b0138234f42396e4784b7fa5d2c81efeac5c4
Gwen Antell <gwenantell@gmail.com>
2022-06-14 17:51:25 +0100

    export env classifier fcn and specify its fcn dependencies

--- R/calc_enviro_variables.R
+++ R/calc_enviro_variables.R
@@ -2,7 +2,9 @@
 # dat can be df in which case xy and env should be col names
 # or, omit dat arg and xy should be coords, env should be vector
 # cutoff is inclusive, e.g. values >= cutoff will be assigned to class
-envirise <- function(grid, dat = NULL, xy, env, cutoff){
+
+#' @export
+classRast <- function(grid, dat = NULL, xy, env, cutoff){
   if (! is.null(dat)){
     xy  <- dat[, xy]
     env <- dat[, env]
@@ -13,38 +15,40 @@ envirise <- function(grid, dat = NULL, xy, env, cutoff){
 
   # tally occurrences with each enviro within raster cells
   lvls <- unique(env)
+  nLvl <- length(lvls)
   lyrL <- lapply(lvls, function(lvl){
     xyMat <- data.matrix( xy[env==lvl,] )
-    rasterize(xyMat, grid, fun = length)
+    terra::rasterize(xyMat, grid, fun = length)
   }  )
-  lyrs <- rast(lyrL)
+  lyrs <- terra::rast(lyrL)
   names(lyrs) <- lvls
 
   if (cutoff <= 0.5){ stop('cutoff must be greater than 0.5') }
   # set NA values to 0 so when summed across layers, result is not NA
   reclass <- matrix(c(NA, 0), ncol = 2)
-  lyrsNoNA <- classify(lyrs, reclass)
+  lyrsNoNA <- terra::classify(lyrs, reclass)
   prop <- lyrs / sum(lyrsNoNA) # convert to proportion
-  clasfy <- rbind(c(0, cutoff, 0), # binarise
-                  c(cutoff, 1, 1))
+  cuts <- rbind(c(0, cutoff, 0), # binarise
+                c(cutoff, 1, 1))
   # values = cutoff classified as above the cutoff (left-closed interval)
-  mjtys <- classify(prop, clasfy,
+  mjtys <- terra::classify(prop, cuts,
                     include.lowest = TRUE,
                     right = FALSE)
-  values(grid) <- NA # ensure blank starting raster
-  for (l in 1:length(lvls)){
-    grid[mjtys[[l]]==1] <- l
+  terra::values(grid) <- NA # ensure blank starting raster
+  for (l in 1:nLvl){
+    grid[ mjtys[[l]]==1 ] <- l
   }
 
   # change NA cells to indet. if they do have occurrence data
-  # but no one environmental class exceeds cutoff
-  anydat <- rasterize(data.matrix(xy), grid)
-  anydat <- classify(anydat, cbind(1, length(lvls)+1))
-  grid <- cover(grid, anydat)
+  # but no single environmental class exceeds cutoff
+  anydat <- terra::rasterize( data.matrix(xy), grid)
+  reclass <- cbind(1, nLvl+1)
+  anydat <- terra::classify(anydat, reclass)
+  grid <- terra::cover(grid, anydat)
 
   lvls <- c(lvls, 'indet.')
-  attrs <- data.frame('num' = 1:length(lvls))
+  attrs <- data.frame('num' = 1:nLvl)
   attrs$mainClass <- lvls
-  levels(grid) <- attrs
+  terra::levels(grid) <- attrs
   return(grid)
 }
